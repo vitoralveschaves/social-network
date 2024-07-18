a2013a0e11f68cbeef7389d42f36fa9e59255e9f
vitoralveschaves <vitorchavesalv@gmail.com>
2024-07-18 18:54:35 -0300

    feat: add findByUsername method in UserRepository interface

--- src/main/java/com/example/springsecurity/repositories/UserRepository.java
+++ src/main/java/com/example/springsecurity/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.springsecurity.repositories;
 import com.example.springsecurity.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
 }
