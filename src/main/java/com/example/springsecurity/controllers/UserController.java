b0a90bbbb4917c685cd5d0fac765922af716e470
vitoralveschaves <vitorchavesalv@gmail.com>
2024-07-18 22:52:16 -0300

    feat: add UserController class and CreateUserDto record

---
+++ src/main/java/com/example/springsecurity/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.example.springsecurity.controllers;
+
+import com.example.springsecurity.controllers.dto.CreateUserDto;
+import com.example.springsecurity.entities.Role;
+import com.example.springsecurity.entities.User;
+import com.example.springsecurity.repositories.RoleRepository;
+import com.example.springsecurity.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/users")
+    @Transactional
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDto createUserDto) {
+
+        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+        var userFromDb = userRepository.findByUsername(createUserDto.username());
+
+        if(userFromDb.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        var user = new User();
+        user.setUsername(createUserDto.username());
+        user.setPassword(passwordEncoder.encode(createUserDto.password()));
+        user.setRoles(Set.of(basicRole));
+
+        userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+}
---
+++ src/main/java/com/example/springsecurity/controllers/dto/CreateUserDto.java
@@ -0,0 +1,4 @@
+package com.example.springsecurity.controllers.dto;
+
+public record CreateUserDto(String username, String password) {
+}
