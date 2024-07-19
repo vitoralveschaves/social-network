0290be17aa09fca1c7f3b6f91b582fa714e7e13e
vitoralveschaves <vitorchavesalv@gmail.com>
2024-07-18 22:24:45 -0300

    feat: add AdminUserConfig class

---
+++ src/main/java/com/example/springsecurity/config/AdminUserConfig.java
@@ -0,0 +1,48 @@
+package com.example.springsecurity.config;
+
+import com.example.springsecurity.entities.Role;
+import com.example.springsecurity.entities.User;
+import com.example.springsecurity.repositories.RoleRepository;
+import com.example.springsecurity.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                user -> {
+                    System.out.println("Admin já existe");
+                },
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(passwordEncoder.encode("123"));
+                    user.setRoles(Set.of(roleAdmin));
+                    userRepository.save(user);
+                }
+        );
+
+    }
+}
