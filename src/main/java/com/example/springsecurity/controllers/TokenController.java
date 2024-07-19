1ed4c06a8f26cefe881af320c9ca829c26c7565e
vitoralveschaves <vitorchavesalv@gmail.com>
2024-07-19 03:42:26 -0300

    feat: add scope of access levels to jwt claims

--- src/main/java/com/example/springsecurity/controllers/TokenController.java
+++ src/main/java/com/example/springsecurity/controllers/TokenController.java
@@ -2,6 +2,7 @@ package com.example.springsecurity.controllers;
 
 import com.example.springsecurity.controllers.dto.LoginRequestDto;
 import com.example.springsecurity.controllers.dto.LoginResponseDto;
+import com.example.springsecurity.entities.Role;
 import com.example.springsecurity.repositories.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @RestController
 public class TokenController {
@@ -40,11 +42,14 @@ public class TokenController {
         var now = Instant.now();
         var expiresIn = 300L;
 
+        var scopes = user.get().getRoles().stream().map(Role::getName).collect(Collectors.joining(" "));
+
         var claims = JwtClaimsSet.builder()
                 .issuer("mybackend")
                 .subject(user.get().getUserId().toString())
                 .issuedAt(now)
                 .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
                 .build();
 
         var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
